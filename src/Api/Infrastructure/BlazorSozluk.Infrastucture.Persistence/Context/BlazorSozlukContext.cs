4a801e9774a4b87e26e59aa62f92a63bce525c81
selcukyazicii <63070617+selcukyazicii@users.noreply.github.com>
2023-05-23 15:15:09 +0300

    ....

--- src/Api/Infrastructure/BlazorSozluk.Infrastucture.Persistence/Context/BlazorSozlukContext.cs
+++ src/Api/Infrastructure/BlazorSozluk.Infrastucture.Persistence/Context/BlazorSozlukContext.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace BlazorSozluk.Infrastucture.Persistence.Context
 {
-    public class BlazorSozlukContext:DbContext
+    public class BlazorSozlukContext : DbContext
     {
         public const string DEFAULT_SCHEMA = "dbo";
 
@@ -22,12 +22,35 @@ namespace BlazorSozluk.Infrastucture.Persistence.Context
         public DbSet<EntryVote> EntryVotes { get; set; }
         public DbSet<EntryFavorite> EntryFavorites { get; set; }
         public DbSet<EntryComment> EntryComments { get; set; }
-        public DbSet<EntryCommentVote> EntryCommentVotes{ get; set; }
+        public DbSet<EntryCommentVote> EntryCommentVotes { get; set; }
         public DbSet<EmailConfirmation> EmailConfirmations { get; set; }
         public DbSet<EntryCommentFavorite> EntryCommentFavorites { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            OnBeforeSave();
+            return base.SaveChangesAsync(cancellationToken);
+        }
+        private void OnBeforeSave()
+        {
+            var addedEntities = ChangeTracker.Entries()
+                .Where(x => x.State == EntityState.Added)
+                .Select(x => (BaseEntity)x.Entity);
+            PrepareAddedEntities(addedEntities);
+        }
+        private void PrepareAddedEntities(IEnumerable<BaseEntity> entities)
+        {
+            foreach (var entity in entities)
+            {
+                entity.CreateDate = DateTime.Now;
+            }
         }
     }
 }

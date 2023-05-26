0ded7dffa094791df45992f7fde91b855867ab95
selcukyazicii <63070617+selcukyazicii@users.noreply.github.com>
2023-05-26 11:50:22 +0300

    generic repository completed

--- src/Api/Core/BlazorSozluk.Api.Application/Interfaces/Repositories/IGenericRepository.cs
+++ src/Api/Core/BlazorSozluk.Api.Application/Interfaces/Repositories/IGenericRepository.cs
@@ -31,5 +31,10 @@ namespace BlazorSozluk.Api.Application.Interfaces.Repositories
         Task<TEntity>GetSingleAsync( Expression<Func<TEntity, bool>>predicate,bool noTracking=true,params Expression<Func<TEntity, object>>[]includes);
         Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate, bool noTracking = true, params Expression<Func<TEntity, object>>[] includes);
         IQueryable<TEntity>Get(Expression<Func<TEntity,bool>>predicate,bool noTracking=true,params Expression<Func<TEntity, object>>[]includes);
+        Task BulkDeleteById(IEnumerable<Guid> ids);
+        Task BulkDelete(Expression<Func<TEntity, bool>> predicate);
+        Task BulkDelete(IEnumerable<TEntity> entities);
+        Task BulUpdate(IEnumerable<TEntity> entities);
+        Task BulkAdd(IEnumerable<TEntity> entities);
     }
 }

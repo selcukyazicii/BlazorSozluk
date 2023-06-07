25785a3fcbf8a7b71e2357a8ea3d7eb278f7f3fc
selcukyazicii <63070617+selcukyazicii@users.noreply.github.com>
2023-06-07 14:44:10 +0300

    ....

--- src/Api/Core/BlazorSozluk.Api.Application/Features/Command/User/Create/CreateUserCommandHandler.cs
+++ src/Api/Core/BlazorSozluk.Api.Application/Features/Command/User/Create/CreateUserCommandHandler.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using AutoMapper;
+using BlazorSozluk.Api.Application.Interfaces.Repositories;
+using BlazorSozluk.Common.Infrastructure.Exceptions;
+using BlazorSozluk.Common.Models.RequestModels;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,23 @@ using System.Threading.Tasks;
 
 namespace BlazorSozluk.Api.Application.Features.Command.User.Create
 {
-    public class CreateUserCommandHandler
+    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, bool>
     {
+        private readonly IMapper _mapper;
+        private readonly IUserRepository _userRepository;
+
+        public CreateUserCommandHandler(IMapper mapper, IUserRepository userRepository)
+        {
+            _mapper = mapper;
+            _userRepository = userRepository;
+        }
+
+        public async Task<bool> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+            var existUser = await _userRepository.GetSingleAsync(x => x.Email == request.EmailAddress);
+            if (existUser is not null)
+                throw new DatabaseValidationException("User already exist");
+            var dbUser = _mapper.Map<Domain.Models.User>(request);
+        }
     }
 }
